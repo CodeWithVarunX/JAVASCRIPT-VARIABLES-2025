b537e13bfb70a7aa785b7594cb15ae5960d75f31
CodeWithVarun_09 <varunvrnvn.09@gmail.com>
2025-03-21 23:05:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// VARIABLES IN JS
+
+// variables are sued to storing data in some reserved keywords like:
+
+// --> let =>
+// it is used for storing data it can update but cannot be redeclared.(it is famous tp storing data).
+
+// --> const =>
+//const used for storing data in memory and it cannot be updated and as long as can't be redeclared.(Still but it is best way to storing data and it is safe for data).
+
+// --> var
+// it s used for storing data also but it can be predefined and can be redeclared and update many times (it's uses less because it's behaviour of changing and updating).
+
+// EXAMPLES
+
+let CityName = "Delhi";
+CityName = "Uttar Pradesh"; // it can updates , but can;t be redeclared
+
+const YourPassword = "1801/101hzbnz#927/iwjdhwhdd$ns"; // cannot be redeclared & updates
+
+var YourName = "XYZ";
+YourName = "ABC"; // it can be updateable
+var YourName = "MNO"; // it can be redeclared
+
+// USING console.table() TO PRINT RESULTS IN TABULAR FORMS
+
+console.table([CityName, YourPassword, YourName]);
+
+{
+  a = 89;
+}
+const a = 80;
+console.log(a);
+
+// this is very basic knowledge about JavaScript Variables
